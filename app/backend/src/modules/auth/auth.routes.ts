61e9abecb2628d5c86373b10060113b1a4ba39a8
danilowskii <danilopaivatiotonho@gmail.com>
2026-03-18 12:06:33 -0300

    fix: added Router type to authRouter at auth.routes.ts

--- app/backend/src/modules/auth/auth.routes.ts
+++ app/backend/src/modules/auth/auth.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { type Request, type Response } from "express";
 import { AuthController } from "./auth.controller.js"
 
-const authRouter = Router();
+const authRouter: Router = Router();
 const authController = new AuthController()
 
 authRouter.post("/auth/register", (req: Request, res: Response) => authController.register(req, res))
